18652ac1c386dddd9edf1f6c86e207a8a5d48643
Ömer Faruk KÖSE <omerfarukose@outlook.com>
2023-09-30 22:13:28 +0300

    Spotify WebView sign in part 1

--- SpotifyApp/Managers/AuthManager.swift
+++ SpotifyApp/Managers/AuthManager.swift
@@ -10,7 +10,21 @@ import Foundation
 final class AuthManager {
     static let shared = AuthManager()
     
+    struct Constants {
+        static let clientID = ""
+        static let clientSecret = ""
+    }
+    
     private init() {}
+    
+    public var signInURL: URL? {
+        let scopes = "user-read-private"
+        let redirectURI = "https://fonts.google.com/"
+        let base = "https://accounts.spotify.com/authorize"
+        let string = "\(base)?response_type=code&client_id=\(Constants.clientID)&scope=\(scopes)&redirect_uri=\(redirectURI)&show_dialog=TRUE"
+        
+        return URL(string: string)
+    }
     
     var isSignedIn: Bool {
         return false
@@ -30,5 +44,20 @@ final class AuthManager {
     
     private var shouldRefreshToken: Bool? {
         return false
+    }
+    
+    public func exchangeCodeForToken(
+        code: String,
+        completion: @escaping ((Bool) -> Void)
+    ) {
+        // Get token
+    }
+    
+    public func refreshAccessToken() {
+        
+    }
+    
+    private func cacheToken() {
+        
     }
 }
